74c45e5051e45977c13046ec509140369fa9a07d
dulecux <dusan.culibrk@live.com>
2021-03-15 19:57:15 +0100

    Client Basket Created

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
@@ -14,21 +15,40 @@ export class ProductDetailsComponent implements OnInit {
   quantity = 1;
 
   constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute,
-    private bcService: BreadcrumbService) {
-    this.bcService.set('@productDetails', ' ')
+              private bcService: BreadcrumbService, private basketService: BasketService) {
+    this.bcService.set('@productDetails', ' ');
   }
 
   ngOnInit(): void {
     this.loadProduct();
   }
 
+  // tslint:disable-next-line: typedef
   loadProduct() {
+    // tslint:disable-next-line: deprecation
     this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe(product => {
       this.product = product;
       this.bcService.set('@productDetails', product.name);
     }, error => {
       console.log(error);
-    })
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  // tslint:disable-next-line: typedef
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  // tslint:disable-next-line: typedef
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
   }
 
 }
